56cf506aab1d6c11d4d2e54adbee41227ac0f67f
Erkan <erkankrgz0707@gmail.com>
2025-06-09 17:37:03 +0300

    Add files via upload

---
+++ car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp18;
+
+namespace ConsoleApp18
+{
+    internal class car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+        private int _doorsNumber;  
+
+        public int doorsNumber
+        {
+            get { return _doorsNumber; }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _doorsNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Kapı sayısı yalnızca 2 veya 4 olmalıdır. Varsayılan olarak -1 atanıyor.");
+                    _doorsNumber = -1;
+                }
+            }
+        }
+
+        public void PrintInfo() // Fixed method name and moved it inside the class  
+        {
+            Console.WriteLine($"Marka: {Marka}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Renk: {Renk}");
+            Console.WriteLine($"Kapı Sayısı: {doorsNumber}");
+        }
+    }
+}
---
+++ carPratic.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp18;
+
+car araba = new car();
+Console.Write("Aracınızın markasını giriniz : ");
+araba.Marka = Console.ReadLine();
+Console.Write("Aracınızın modelini giriniz : ");
+araba.Model = Console.ReadLine();
+Console.Write("Aracınızın rengini giriniz : ");
+araba.Renk = Console.ReadLine();
+Console.Write("Aracınızın kapı sayısını giriniz (2 veya 4): ");
+araba.doorsNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Aracınızın bilgileri aşağıdadır...");
+araba.PrintInfo(); 
